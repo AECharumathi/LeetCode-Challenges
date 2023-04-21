2a53f4380e03e90d652b6b8351ee7239ab00edc7
Charumathi A E <charumathielangovan96@gmail.com>
2023-04-21 15:28:56 -0700

    Time: 84 ms (6.90%), Space: 43.2 MB (72.12%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -4,8 +4,6 @@
  */
 var compose = function(functions) {
 	return function(x) {
-        if(functions.length <= 0 ) return x;
-        
         functions.reverse().forEach((func)=> x=func(x));
         return x;
     }
