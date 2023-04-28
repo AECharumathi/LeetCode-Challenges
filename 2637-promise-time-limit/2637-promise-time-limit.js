48ba433d3f2b75a214f1109ebcb959ad1204f020
Charumathi A E <charumathielangovan96@gmail.com>
2023-04-28 12:29:28 -0700

    Time: 59 ms (65.69%), Space: 42 MB (59.54%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.js
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -5,7 +5,6 @@
  */
 var timeLimit = function(fn, t) {
 	return async function(...args) {
-        //const func = new Promise((resolve)=> resolve(fn(...args)));
         const func = fn(...args);
         const timeLimit = new Promise((_,reject)=>
             setTimeout(()=>reject("Time Limit Exceeded","time"), t)
