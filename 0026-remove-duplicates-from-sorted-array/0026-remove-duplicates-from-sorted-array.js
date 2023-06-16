5435833d0b316ee4067aaf5deca22a0c8b4dba87
Charumathi A E <charumathielangovan96@gmail.com>
2023-06-16 11:41:54 -0700

    Time: 135 ms (15.69%), Space: 45.3 MB (11.65%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -5,10 +5,12 @@
 var removeDuplicates = function(nums) {
    let i = 0 ;
         while( i < nums.length-1 ){
-            if(nums[i] === nums[i+1]){
-                nums.splice(i+1,1);
+            let j = i+1;
+            if(nums[i] === nums[j]){
+                nums.splice(j,1);
             }else{
                 i++;
             }
-        }    
+        }  
+    return nums.length;
 };
