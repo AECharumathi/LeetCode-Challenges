0aadf1b6c9c2d30ddc27bdd48bbedf353c5d83a8
Charumathi A E <charumathielangovan96@gmail.com>
2023-07-31 11:53:34 -0700

    Time: 387 ms, Memory: 51.3 MB - LeetHub

---
+++ 3sum/3sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> triplets= new HashSet<>();        
+        Arrays.sort(nums);
+        for(int i=0; i< nums.length-2; i++){
+           if(nums[i] > 0) 
+                return new ArrayList<>(triplets);
+           int j = i+1, k = nums.length-1;
+           while(j < k ){
+              int sum = nums[i] + nums[j] + nums[k];
+            
+              if(sum == 0){
+                 triplets.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                 j++;
+                 if(nums[j] == nums[j-1] && j < k)
+                   j++;   
+              }else if( sum < 0){
+                  j++;
+              }else{
+                  k--;
+              }
+           }
+        }
+        
+        return new ArrayList<>(triplets);
+    }
+
+}
