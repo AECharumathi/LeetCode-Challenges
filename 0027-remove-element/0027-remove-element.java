90a06e7ff31d839117f9c0bb2a159b099c60044c
Charumathi A E <charumathielangovan96@gmail.com>
2023-08-14 18:06:15 -0700

    Time: 0 ms (100.00%), Space: 40.9 MB (73.07%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -2,13 +2,11 @@ class Solution {
     public int removeElement(int[] nums, int val) {
         int k = 0;
         for(int i = 0 ; i< nums.length; i++){
-            if(nums[i] == val){
-                nums[i] = 999;
-            }else{
+            if(nums[i] != val){
+                nums[k] = nums[i];
                 k++;
             }
         }
-        Arrays.sort(nums);
         return k;
     }
 }
