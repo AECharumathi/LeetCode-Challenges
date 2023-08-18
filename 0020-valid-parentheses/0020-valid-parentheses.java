467e97a29a77d9c7b6d38ae26eb27424db650f54
Charumathi A E <charumathielangovan96@gmail.com>
2023-08-17 17:24:25 -0700

    Time: 2 ms (88.48%), Space: 40.7 MB (41.81%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for(int i =0; i< s.length(); i++){
+            if(s.charAt(i)== '(' || s.charAt(i)=='[' || s.charAt(i) == '{'){
+                stack.push(s.charAt(i));
+            }else if( stack.size() > 0){
+                if((s.charAt(i) == ')' && stack.peek() == '(') ||(s.charAt(i) == ']' && stack.peek() == '[') || (s.charAt(i) == '}' && stack.peek() == '{')  ){
+                stack.pop();
+            }else return false;
+            }else{
+                return false;
+            }
+        }
+        if(stack.size() == 0 ) return true;
+        else return false;
+        
+    }
+}
